1d8868790fbc78088dfc75036b891651c2c96eb9
Sinan Kara <snan_kara_@hotmail.com>
2021-01-17 21:42:52 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        //Temel Operasyonlar - CRUD Operasyonları
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " - " + customer.Age + " - " + customer.Adress);
+            Console.WriteLine("Added!");
+        }
+
+        public void Delete(Customer customer, int Id)
+        {
+            string message = customer.Id == Id ? customer.FirstName + " is deleted!" : "Exception!";
+            Console.WriteLine(message);
+
+        }
+
+        public void Update(Customer customer, int Id)
+        {
+            string message = customer.Id == Id ? customer.FirstName + " is updated!" : "Exception!";
+            Console.WriteLine(message);
+        }
+
+        public void GetList(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.FirstName + " " + customer.LastName + " - " + customer.Age + " - " + customer.Adress);
+            }
+
+            Console.WriteLine("Listed!");
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "Sinan";
+            customer1.LastName = "KARA";
+            customer1.Age = 22;
+            customer1.Adress = "Istanbul - Maltepe";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.FirstName = "Hande";
+            customer2.LastName = "Sayek";
+            customer2.Age = 30;
+            customer2.Adress = "Istanbul - Kartal";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Mehmet";
+            customer3.LastName = "Gencer";
+            customer3.Age = 25;
+            customer3.Adress = "Antalya - Merkez";
+
+            Customer[] customers = { customer1, customer2, customer3 };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+            customerManager.GetList(customers);
+            customerManager.Update(customer1, customer1.Id);
+            customerManager.Delete(customer2, customer2.Id);
+        }
+    }
+}
